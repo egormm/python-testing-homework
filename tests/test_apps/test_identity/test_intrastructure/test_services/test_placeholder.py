050c3f9ddd83320070c88970ecbc2f269bca8981
e.makrushin <e.makrushin@cian.ru>
2023-04-01 13:45:18 +0400

    fix ci

--- tests/test_apps/test_identity/test_intrastructure/test_services/test_placeholder.py
+++ tests/test_apps/test_identity/test_intrastructure/test_services/test_placeholder.py
@@ -1,15 +1,18 @@
 from typing import TYPE_CHECKING
 
-from server.apps.identity.intrastructure.services.placeholder import _serialize_user
+from server.apps.identity.intrastructure.services import placeholder
 from server.apps.identity.models import User
 
 if TYPE_CHECKING:
     from plugins.identity.user import UserModelFactory
 
 
-def test_serialize_user_no_date_of_birth(user_model_factory: 'UserModelFactory') -> None:
+def test_serialize_user_no_date_of_birth(
+    user_model_factory: 'UserModelFactory',
+):
+    """Test serialize user with no date of birth."""
     user = User(**user_model_factory(date_of_birth=None))
 
-    serialized_user = _serialize_user(user)
+    serialized_user = placeholder._serialize_user(user)  # noqa: WPS437
 
     assert serialized_user['birthday'] == ''
